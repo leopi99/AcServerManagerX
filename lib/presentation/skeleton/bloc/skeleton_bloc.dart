1f66f148685cb00ce2454a7b73fa36ec6fe0b0b8
Leonardo Pizio <pizio.leonardo@gmail.com>
2022-02-08 15:23:17 +0100

    Can open/close the pane

--- lib/presentation/skeleton/bloc/skeleton_bloc.dart
+++ lib/presentation/skeleton/bloc/skeleton_bloc.dart
@@ -2,9 +2,11 @@ import 'package:rxdart/rxdart.dart';
 
 class SkeletonBloc {
   final BehaviorSubject<int> _currentPageSubject = BehaviorSubject.seeded(0);
+  final BehaviorSubject<bool> _panelOpenSubject = BehaviorSubject.seeded(true);
 
   Stream<int> get currentPage => _currentPageSubject.stream;
+  Stream<bool> get panelOpen => _panelOpenSubject.stream;
 
   void changePage(int index) => _currentPageSubject.add(index);
-  int get currentPageSync => _currentPageSubject.value;
+  void changePaneOpen(bool open) => _panelOpenSubject.add(open);
 }
--- lib/presentation/skeleton/presentation/skeleton_page.dart
+++ lib/presentation/skeleton/presentation/skeleton_page.dart
@@ -1,6 +1,7 @@
 import 'package:acservermanager/presentation/homepage/homepage.dart';
 import 'package:acservermanager/presentation/skeleton/bloc/skeleton_bloc.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as material;
 
 class SkeletonPage extends StatelessWidget {
   SkeletonPage({Key? key}) : super(key: key);
@@ -13,30 +14,54 @@ class SkeletonPage extends StatelessWidget {
       stream: _bloc.currentPage,
       initialData: 0,
       builder: (context, snapshot) {
-        return NavigationView(
-          content: NavigationBody(
-            index: snapshot.data!,
-            children: const [
-              Homepage(),
-              Homepage(),
-            ],
-          ),
-          pane: NavigationPane(
-            onChanged: (index) {
-              _bloc.changePage(index);
-            },
-            selected: snapshot.data!,
-            items: [
-              PaneItem(
-                icon: const Icon(FluentIcons.home),
-                title: const Text('Home'),
+        return StreamBuilder<bool>(
+          stream: _bloc.panelOpen,
+          initialData: true,
+          builder: (context, panelOpenSnapshot) {
+            return NavigationView(
+              content: NavigationBody(
+                index: snapshot.data!,
+                children: const [
+                  Homepage(),
+                  Homepage(),
+                ],
               ),
-              PaneItem(
-                icon: const Icon(FluentIcons.home),
-                title: const Text('Home'),
+              pane: NavigationPane(
+                onChanged: _bloc.changePage,
+                menuButton: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: panelOpenSnapshot.data!
+                        ? MainAxisAlignment.start
+                        : MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: const Icon(material.Icons.menu),
+                        onPressed: () {
+                          _bloc.changePaneOpen(!panelOpenSnapshot.data!);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                displayMode: panelOpenSnapshot.data!
+                    ? PaneDisplayMode.open
+                    : PaneDisplayMode.compact,
+                selected: snapshot.data!,
+                items: [
+                  PaneItem(
+                    icon: const Icon(FluentIcons.home),
+                    title: const Text('Home'),
+                  ),
+                  PaneItem(
+                    icon: const Icon(FluentIcons.home),
+                    title: const Text('Home'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
