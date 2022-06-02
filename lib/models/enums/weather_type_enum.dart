02654806dcdf6c02d5f9c2904e37991c21c21b0e
Leonardo Pizio <pizio.leonardo@gmail.com>
2022-06-02 18:34:54 +0200

    Environment sdk min at 2.17
    Used to implement better enums

--- lib/models/enums/weather_type_enum.dart
+++ lib/models/enums/weather_type_enum.dart
@@ -2,41 +2,14 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 enum WeatherTypeEnum {
-  clear,
-  heavyClouds,
-  heavyFog,
-  lightClouds,
-  lightFog,
-  midClear,
-  midClouds,
-}
+  clear(WeatherIcons.day_sunny),
+  heavyClouds(WeatherIcons.cloudy),
+  heavyFog(WeatherIcons.fog),
+  lightClouds(WeatherIcons.cloudy),
+  lightFog(WeatherIcons.fog),
+  midClear(WeatherIcons.day_sunny),
+  midClouds(WeatherIcons.night_partly_cloudy);
 
-extension WeatherTypeEnumExtension on WeatherTypeEnum {
-  IconData get icon {
-    late IconData icon;
-    switch (this) {
-      case WeatherTypeEnum.clear:
-        icon = WeatherIcons.day_sunny;
-        break;
-      case WeatherTypeEnum.heavyClouds:
-        icon = WeatherIcons.cloudy;
-        break;
-      case WeatherTypeEnum.heavyFog:
-        icon = WeatherIcons.fog;
-        break;
-      case WeatherTypeEnum.lightClouds:
-        icon = WeatherIcons.cloudy;
-        break;
-      case WeatherTypeEnum.lightFog:
-        icon = WeatherIcons.fog;
-        break;
-      case WeatherTypeEnum.midClear:
-        icon = WeatherIcons.day_sunny;
-        break;
-      case WeatherTypeEnum.midClouds:
-        icon = WeatherIcons.night_partly_cloudy;
-        break;
-    }
-    return icon;
-  }
+  const WeatherTypeEnum(this.icon);
+  final IconData icon;
 }
